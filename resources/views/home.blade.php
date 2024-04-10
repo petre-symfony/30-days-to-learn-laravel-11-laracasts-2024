65991b1a3c7d3c3c9c9bcf87e8f8bf34aa2c83b3
petrero <petrero@gmail.com>
2024-04-10 09:40:05 +0300

    3.5 And now, yeah, we can upgrade our code base. So I will create a new directory here, and I'm going to call it components. This directory name is important, by the way. If you're not familiar with that term, think of it as any kind of reusable block that could be referenced in multiple places around your application. A task, a menu, a dropdown item, a layout file, a card, a message, an avatar, any of these would be just fine.
     So notice how I said layout. Well, layout is a component. And when we create a file within this directory, it's a signal to Laravel that it should be treated as a component. So let's do that now. And again, don't forget to add that blade suffix there.
     A lot of people forget that, at least initially. All right. So what I will do is I will take all of this wrapping code, and I will instead place it within this layout file here.
     All right. So that looks pretty good to me, except for this final line here that, of course, is unique to the homepage. But we'll fix that in just a minute. So here's what I'll do. I'm going to return to my home view, delete it entirely. So now if I come back to Safari and reload the homepage, of course, I don't see anything at all, and that would make sense.
     But now I want to reference our layout file. And we do it like this. We interact with it as if it were a custom HTML tag. So what's the name of the tag? Well, you gave it a name when you created the file, in this case, layout.
     So we always start components with x- to ensure that they're unique and they don't interfere with existing HTML tags. And then we reference the name of the component. All right. And that's it. This is really cool. So if I come back and give it a refresh, sure enough, we are loading that layout file, which is great. All right.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Home Page</title>
-</head>
-<body>
-  <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-  </nav>
-  Hello from the Home Page
-</body>
-</html>
+<x-layout></x-layout>
