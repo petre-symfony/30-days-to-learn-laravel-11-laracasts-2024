3fa8db481339567bf9ec81bea780b0da8bedb2bb
petrero <petrero@gmail.com>
2024-04-10 10:18:42 +0300

    3.7 I can replace the contact page with hello from the contact page, and then about, and we are removing all of that duplication, which is really great.
     All right. So come back, give it a refresh, home, about, contact. And now, if I ever want to add a fourth page, maybe something like meet the team, notice I can update the navigation area within a single file rather than manually updating every single view where we pasted that markup.
     It's clearly the way to go. But I'll come back and remove that. Okay.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title>About Page</title>
-	</head>
-	<body>
-    <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-    </nav>
-		Hello from the About Page
-	</body>
-</html>
+<x-layout>
+	Hello from the About Page
+</x-layout>
