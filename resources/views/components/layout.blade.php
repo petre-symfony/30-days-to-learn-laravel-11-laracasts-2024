9cb31514c77a3411464587b13e956c2c85139c65
petrero <petrero@gmail.com>
2024-04-11 09:57:15 +0300

    4.7 And if I scroll down here, there we are. And then notice it says your content here. So example content goes here. Or the slotted content, right? And we learned about that in the last episode.  There you go. So this would be the main content area for each individual page.  So let's see. Can we just swap that out with slot? Alright, let's give it a shot. So we come back, give it a refresh. And now notice a quick note. It's going to appear as if we're on the homepage because those CSS styles are hard-coded on that home link. But we will make that dynamic shortly.  Anyways, right now we're clearly on the contact page, as you can see here. And then we're on the about page. And then we're on the homepage.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -109,7 +109,7 @@
     </header>
     <main>
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        Slotted content goes here
+        {{ $slot }}
       </div>
     </main>
   </div>
