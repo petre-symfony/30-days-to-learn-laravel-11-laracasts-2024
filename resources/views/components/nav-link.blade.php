24f94599ebb4cb22c82efeb6f6799c29624bfb98
petrero <petrero@gmail.com>
2024-04-12 09:48:09 +0300

    5.12 Keep in mind, you can also do things like this, where you could declare a PHP directive. And we could do nphp to create our block effectively. And then within here, you could have some kind of code, you could do a conditional, you could inspect the value of active and then tweak it in certain situations. And all of that is isolated within this single file component, which I think is really cool. All right, and that does it. So you know what, really good job. I would imagine for many of you, this is sort of foreign territory.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,8 @@
 @props(['active' => false])
+
+@php
+  
+@endphp
 <a
     class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
     aria-current="{{ $active ? 'page' : 'false' }}"
