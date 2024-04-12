b9123a2dfc73f73cd0fb62c49b2f3ab6faff2ad7
petrero <petrero@gmail.com>
2024-04-12 09:19:40 +0300

    5.7 Next, the current URI. There's a number of ways we could handle this. Of course, we could inspect the href. But another way I want to show you is to introduce a prop that indicates whether this nav link should be marked as the active nav link. And actually, real quick, let's go over that. So within Blade Components, you will hear about attributes and then props. So attributes represents HTML attributes, the href, the ID, the class, anything. A prop would be anything that is not an attribute. And we basically distinguish between them because you have to know, well, should that prop be included as one of the attributes that we echo as part of the anchor tag? Well, no, right? So if you have a prop called active, and you echo it out as part of the attributes, now you have an anchor tag with an attribute name of active. And that doesn't make sense. So we have to distinguish between the two. And here's how we do that. At the top of my Blade Components, I'm going to use a blade directive. You'll know blade directives because they begin with an at symbol. And notice how my editor autocompletes all of the available options here. And there's countless ones. Again, don't feel overwhelmed. You will use a fraction of these. But you have directives like if, or unless, or for each, or dump to quickly dump something to the screen. Think of them as little bits of shorthand or sugar that ultimately compile down to vanilla PHP, echo statements or function calls. So in our case, we want a new prop. So we can declare our props as an array. And we've decided we want a prop called active. Okay, so I'm going to show you how this works. And this will really help, I think. We're going to spit out all of the attributes that might be potentially passed in as a nav link. So for example, in this one, I'll set ID equals something here, just so we can see it in effect. Alright, let's come back to the browser, give it a refresh, and then inspect this. Alright, and notice it does include the href and the ID. And it does that because we spit out or echoed all of the attributes here. So if I remove that and give it a refresh, of course, we're not going to see them.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,5 +1,8 @@
+@props(['active'])
 <a
     class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-    aria-current="{{ request()->is('/') ? 'page' : 'false' }}">
+    aria-current="{{ request()->is('/') ? 'page' : 'false' }}"
+    {{ $attributes }}
+>
   {{ $slot }}
 </a>
