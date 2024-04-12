4184c96131730ec9fcb1e4c79c10e466a367a2d1
petrero <petrero@gmail.com>
2024-04-12 09:45:15 +0300

    5.11 All right, so now that I can provide an expression, I can bring this back to what we originally had, which was request is contact in this case. So we're going to copy this. And now this one should be treated as active if the request is about. And then for home, we check if the request is forward slash. And yeah, I think that should do it. So we have contact about home, everything's working. And now you know what, you've actually learned surprisingly a great deal about blade components, even before you learned about eloquent. And that's, that's a very interesting way to learn. But that's just how it happened to work out. And I think that's a good thing, actually. So now you have a dedicated blade component, you've learned about attributes, you've learned about props.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,9 +19,9 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <x-nav-link href="/">Home</x-nav-link>
-                <x-nav-link href="/about" :active="true">About</x-nav-link>
-                <x-nav-link href="/contact">Contact</x-nav-link>
+                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
               </div>
             </div>
           </div>
