ab7d98faae9aa7a7dcac8fc38b5c35c06b733cd9
petrero <petrero@gmail.com>
2024-04-13 11:47:37 +0300

    6.8 Okay, but happy path first. So now I'm going to return job. And I will pass through our job to the view. Finally, right here, we can use the fully qualified class path. But instead, I'm going to import it. So you can do it manually or use whatever shortcuts your editor provides. In PHP storm, I can press option return and do cleanup code. And now notice it automatically imports that at the top. Okay, so are we on the same page, we have a new route to show a job. Right now we're just duplicating the list of jobs, but we'll fix that soon, I promise. And then we search for the job that has the matching ID that's included as part of the URI. Once it finds it, and this is happy path, so we assume that we're going to find it, we load a view.   Alright, let's go into our view now. And it looks like we don't have one yet. So I will copy jobs, paste it and call this one job that played that PHP. And then down here, yeah, maybe whatever you want, we could have an h2, this would be job title. And then we don't have much else we could do a salary. This job pays job salary per year. And that's good enough for a little demo here. Real quick, let's add a couple classes here. These are tailwind classes, text large, you can leave it blank if you want. But yeah, we're just making the font bold and the text slightly large. Easy enough for me. Alright, back to the browser. And there we go. Here's our director position. What do we have here job with an ID of one. Here's number two for a programmer, three for a teacher, it all works.

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,8 @@ Route::get('/jobs/{id}', function ($id) {
 	];
 
 	$job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-	dd($job);
 
-	return view('contact');
+	return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
