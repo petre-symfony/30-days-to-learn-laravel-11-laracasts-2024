e506aa3424d4895a2220f273e9ad6e79e2cbbfc9
petrero <petrero@gmail.com>
2024-04-13 11:51:23 +0300

    6.9 So now think about it, all you have to do is link to that specific page within your job listings view. So right here, let's make it dynamic job ID. Let's see if it works. So here's our jobs page. We don't have any styling yet. But notice they are clickable. So here's our director position. Let's go back. Here's our programmer position. And here is our teacher position. So yeah, the last thing I'm going to do is just add, I don't know, text blue, hover, underline for each of these, just to make it not great, but just to make it a little more clear that these are clickable links. Hmm, maybe I should be a teacher. All right, this job pays 0,000.   Here's all the details. Here's a form I can fill out to learn more. We are well on our way to creating a jobs board. All right, so yeah, we're not actually going to build the real jobs board platform until the end of the course when you feel quite a bit more comfortable. But yeah, throughout the series, you're going to see little nuggets, little traces of concepts that we might use once we start building that thing officially. So you're doing great work. This video went a little long, so no homework today. I will see you in day seven. Bye.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,7 @@
 
 	<ul>
 		@foreach($jobs as $job)
-			<a href="/jobs/">
+			<a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
 				<li><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year</li>
 			</a>
 		@endforeach
