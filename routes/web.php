f176d9ac14dd118cb618e924bd14047e122f5519
petrero <petrero@gmail.com>
2024-04-13 11:33:52 +0300

    6.6 Okay, so now think about it. I'm going to duplicate this jobs listing. But again, we could extract it and later we're going to do that. Because a bunch of duplicated jobs isn't a great idea. But yeah, what I could do is have our list of jobs here. And then all we have to do is say, well, give me the job where the ID equals the one that you passed in. So why don't we say job equals and do you know how to do this? Well, you could do there's a number of ways to do it. You could do a for each loop over each of the items and then manually check is the ID of that item equal to the ID that we're trying to reference? If so, that's the one you want. But as it turns out, you're using a framework. So you should leverage the framework. I will teach you about collections later in the series. But for now, it would be helpful to know that Laravel includes an ARR class. And that stands for array, of course. And this gives you access to countless methods, as you can see here, to interact with arrays. So if I want to find the first item within an array that matches some kind of criteria, I could use the first function. I give it our array, and then I provide a function. And this function will be called for each item within the array, and it will receive the current item. So in this case, it's going to loop over the jobs. And for each item, it will pass that into your function. So job would be equal to this on the first iteration, then this one, then this one, you get it. Okay, so now this should return a Boolean that indicates, is this the one you mean? Is this the first one you're trying to find? So in our case, we're going to say return job ID equals the ID that was passed in. But as you may know, because we're in a closure here, I don't have access to the ID here. So you have two ways to solve this. One, with traditional PHP, you would add use. Use the ID, make that available here. And that would do the trick.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -29,7 +30,28 @@ Route::get('/jobs', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-	dd($id);
+	$jobs = [
+		[
+			'id' => 1,
+			'title' => 'Director',
+			'salary' => '$50.000'
+		],
+		[
+			'id' => 2,
+			'title' => 'Programmer',
+			'salary' => '$10.000'
+		],
+		[
+			'id' => 3,
+			'title' => 'Teacher',
+			'salary' => '$40.000'
+		]
+	];
+
+	Arr::first($jobs, function ($job) use ($id){
+		return $job['id'] === $id;
+	});
+
 	return view('contact');
 });
 
