d2d1e0545f14f82587fc6bf16bf1bdf9fcb5bebb
petrero <petrero@gmail.com>
2024-04-15 11:30:51 +0300

    10.4 You'll remember, excuse me, let's go into user factory. Okay, so you'll remember there's an additional method called unverified. And it looks like if we want a user in an unverified state, that would mean email verify that is set to null. But notice by default up here, it's just set to now. So if we switch back, all of these users, where's it will have the email verified ads, timestamp set. Okay, so how do we activate this particular state? And that would be the term, by the way that we use states? Well, like this app calls user factory, and then we call the state method, which is unverified. Create. App\Models\User::factory()->unverified()->create()  And that's it. So notice email verify that is set to no. So yeah, you're not going to reach for this all the time. But trust me, once you start building more substantial projects, you'll run into situations like this where, yeah, you need to create a model or a record that's in a very particular state, usually for the purposes of performing a test. So I'll let you take a look at this. Notice you create a method, give it any name you want.  And then within that method, you call a state method on the object, where you pass it a closure or a function that returns an array. Okay, so you can create these yourself as well. For example, often, we'll just do this as a quick exercise. Often for simple projects, your user table might have an admin status. And let's say by default, it's set to false, you're not an admin. Yeah, maybe in certain situations, you want to say, well, given I have a user who is an administrator, when I bada bing bada boom, then I should zip zap zap, right? So yeah, that would be a good use case for a state. So I could just copy what I have here and say, okay, well, this one, this state will be called admin. And this will set the admin status to true. All right, and that's it. Now you have a new piece of state. So you would say user::factory()->admin()->create(). And now that will use all of this data. But then it's effectively going to grab these attributes and merge them and override the defaults. And that would be one way to deal with this. Lovely.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,15 +29,16 @@ class UserFactory extends Factory {
 			'email_verified_at' => now(),
 			'password' => static::$password ??= Hash::make('password'),
 			'remember_token' => Str::random(10),
+			'admin' => false
 		];
 	}
 
 	/**
 	 * Indicate that the model's email address should be unverified.
 	 */
-	public function unverified(): static {
+	public function admin(): static {
 		return $this->state(fn(array $attributes) => [
-			'email_verified_at' => null,
+			'admin' => true,
 		]);
 	}
 }
