d822d8bde995d458903abb77e2bb06811657b796
petrero <petrero@gmail.com>
2024-04-15 12:12:54 +0300

    10.7 However, another option is to use this table foreign ID for and notice right here, the argument it expects is an eloquent model. So I could say, well, follow a convention and create the necessary foreign ID for an employer. And that would be another way to do it.  Alright, so let's give this a shot. Now, because we've made some changes, and we're so early in the project, I will often run migrate fresh, just drop everything and run it from scratch.  Alright, so now we've built up all of our tables from the beginning. Okay, so if we come back, give it a refresh, of course, we've lost all of our seed data, and that's fine. But if we have a look at job listings, we now have an employer ID, a title and a salary, and the employer ID refers to this new employers table. Alright, so now, if we go back to our job factory, this is no longer enough. And in fact, if we try it out, where are we? There we go. Let's try to create 300 jobs. It doesn't work. Because of course, employer ID was not provided.

--- database/migrations/2024_04_13_155820_create_job_listings.php
+++ database/migrations/2024_04_13_155820_create_job_listings.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +12,7 @@ return new class extends Migration {
 	public function up(): void {
 		Schema::create('job_listings', function (Blueprint $table) {
 			$table->id();
-			$table->unsignedBigInteger('employer_id');
+			$table->foreignIdFor(Employer::class);
 			$table->string('title');
 			$table->string('salary');
 			$table->timestamps();
