7fbf3cfeb59c8f50107cbc0e277d6dbb7b5b5e67
petrero <petrero@gmail.com>
2024-04-16 12:30:32 +0300

    13.6 OK, so if we want to remove the screen, we have to solve the problem. So we solve it by eager loading the relationship.     So once again, job with the relationship name, get me the results. And that's it. So we come back, give it a refresh, and we have solved that problem. OK, so now we are eager loading our employer relationship. We have minimized the number of SQL queries. I think we're in really good shape. So in the next episode, we will move on to day 14. I'll see you then.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,6 @@ class AppServiceProvider extends ServiceProvider {
 	 * Bootstrap any application services.
 	 */
 	public function boot(): void {
-		Model::preventLazyLoading();
+		//Model::preventLazyLoading();
 	}
 }
