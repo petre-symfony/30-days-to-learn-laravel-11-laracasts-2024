57263fdd600da4b758b127657aff5bdd8b52ac4b
petrero <petrero@gmail.com>
2024-04-16 11:58:43 +0300

    13.1 Eager Loading and the N+1 Problem  All right, folks, welcome to day 13. Today, we'll have a look at the n plus one problem.  Let's review an example. So in my routes file, we have an endpoint for jobs. This fetches all jobs from the database and then loads a jobs view. I can command click on that in my editor to go directly to it. Now, if we have a look at this in the browser, this is what we get. Not the prettiest thing in the world. So you know what? Why don't we take 60 seconds and turn each of these items into a card of sorts? To start, let's swap out the unordered list for a simple div. And then for each card, this could be a div, it could be an article, or it could even be an anchor tag. That way, the entire card is clickable.  So why don't we try that out? I'm going to make the anchor tag a block level element. And then what we'll do is maybe add some padding on the left and right, a little bit more on the top and bottom. And maybe we'll have a border and then maybe a color of border gray 200, maybe something like that. Okay, next, we don't want blue text. So I will delete that entirely. And I will remove the hover styling. All right, let's see what we have. So back to the browser, give it a refresh. And yeah, that's looking just a little bit better. The only remaining thing might be to make each of them rounded. And then I want a bit of space in between each one. So I'll show you a little trick. On the parent element, you could do something like space-y-4. And then I'll add just a little bit of margin in between each of the individual items. And I think that looks fine. Okay, so now notice the entire thing is clickable. And it takes me to the actual job page.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
 	<x-slot:heading>Job Listing</x-slot:heading>
 
-	<ul>
+	<div class="space-y-4">
 		@foreach($jobs as $job)
-			<a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-				<li><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year</li>
+			<a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+				<strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
 			</a>
 		@endforeach
-	</ul>
+	</div>
 </x-layout>
