a2d9c949d902d134c278138bbef75b83f0fcf512
petrero <petrero@gmail.com>
2024-04-16 12:07:20 +0300

    13.2 Cool. Alright, so now the next step is, I'd like to display the employer right above the job title here. So let's do that. Okay, so I will select this. And why don't we wrap it, it could be a span, I'm just going to do a simple div here. It's not really a paragraph. So I wouldn't do that. Maybe there's an argument for that, though. And then above it, we'll have another div where I echo the job employer. So there's our belongs to relationship, and then the name of the employer.  Okay, let's have a look. Come back, give it a refresh. All right. Next, why don't we add a little emphasis. So let's make it bold. I'm going to make it blue. It's not a link currently, but it eventually would be, wouldn't it? So that's okay. And at that point, it does become a link, we'll swap that out with an anchor tag. Okay, so now for every job listing, we can see the employer's name, the job title, and the salary. It's looking pretty good.  Okay, but now we have introduced a bit of a problem. And that problem is called the n plus one problem. Okay, so what's the issue? Well, do you remember an episode or two ago, where I mentioned that when you reference the relationship, a new SQL query will be performed, and I called that lazy loading? All right, well, how does lazy loading work within the context of a loop? Could we end up in a situation where for each item in the loop, you execute another SQL query to load the employer? Could that happen? And the answer is 100% yes. And that's specifically where the name N plus one comes from.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,7 +4,8 @@
 	<div class="space-y-4">
 		@foreach($jobs as $job)
 			<a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-				<strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year
+				<div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+				<div><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year</div>
 			</a>
 		@endforeach
 	</div>
