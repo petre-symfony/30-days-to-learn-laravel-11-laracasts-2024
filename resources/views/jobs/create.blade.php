d3512fbe7db33acc4d97cd04ea28d008ac7c8bc0
petrero <petrero@gmail.com>
2024-04-17 16:41:00 +0300

    16.7 Okay, so do I need a text area? Actually, no, in this case. So what I will do is duplicate this, and then down here, I can select the text area entirely and delete it. Alright, so this next one will be salary.   So I'm going to select all of this. Salary. Update this here. And real quick, autocomplete, that's irrelevant in this case. It would make sense for a username or an email, but for title and salary, it's not useful. So I can get rid of that. Alright, next, placeholder. Again, it's going to be something like 0,000 per year. It's whatever the employer wants to add there.   Alright, come back, give it a refresh. And that's looking pretty good. We have our buttons.   The only remaining step is the profile information at the top. So I'll say, create a new job.   We just need a handful of details from you. Alright, I like it. So now, if I add some dummy data here and I click save, well, nothing happens. And of course, that would make sense if I switch back. And real quick, I can get rid of the comments here because I'm using the CDN.   But yeah, anyways, the form is blank. So if we have a look, it's making a GET request to itself and passing through those fields. And that's not what we want.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -5,11 +5,10 @@
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">
-          Profile
+          Create a New Job
         </h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">
-          This information will be displayed publicly so be careful what
-          you share.
+          We just need a handful of details from you
         </p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -23,7 +22,7 @@
                     focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md
                   "
               >
-                <input type="text" name="title" id="title" autocomplete="title"
+                <input type="text" name="title" id="title"
                   class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900
                     placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6
                   "
@@ -33,21 +32,24 @@
             </div>
           </div>
 
-          <div class="col-span-full">
-            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
+          <div class="sm:col-span-4">
+            <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">
+              Salary
+            </label>
             <div class="mt-2">
-              <textarea id="about" name="about" rows="3"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1
-                  ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
-                  focus:ring-indigo-600 sm:text-sm sm:leading-6
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2
+                  focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md
                 "
               >
-
-              </textarea>
+                <input type="text" name="salary" id="salary"
+                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900
+                    placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6
+                  "
+                  placeholder="$50.000 Per Year"
+                >
+              </div>
             </div>
-            <p class="mt-3 text-sm leading-6 text-gray-600">
-              Write a few sentences about yourself.
-            </p>
           </div>
 
         </div>
