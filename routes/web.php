d16c9a2ff3d4e46e4cce26810999ce1242d8d885
petrero <petrero@gmail.com>
2024-04-17 15:56:16 +0300

    16.3 But now think about it. Jobs slash jobs doesn't really work. Well, let's rename this. Instead of jobs, I will call it index. And again, this is a common convention. The view and the action for displaying all of a resource is typically called index. Next, the view for displaying or showing a single resource or record is called show. So I will rename this to show.blade.php. Alright, so you're going to repeat this for all of your resources. If you have tasks, then you would have a tasks folder in your view. You'd have an index view for displaying all tasks, a show view for displaying a single task, a create view — and I'll create that right now — for creating a task or creating a job, as we're doing in this example. Okay, so now let's go back and adjust our routes. And you can see, because I'm using a plugin called Laravel Idea, it immediately notifies me that this view no longer exists. Alright, so I could say jobs slash index, and that would work. However, you can also use dot notation in place of a forward slash for the directory separator. So this is a little bit more common. Alright, this one would be jobs dot create. And notice I have auto-completion here, which is great. And then finally, we have jobs dot show. Alright, this feels a little better to me.

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,20 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
 	$jobs = Job::with('employer')->cursorPaginate(3);
-	return view('jobs', [
+	return view('jobs.index', [
 		'jobs' => $jobs
 	]);
 });
 
 Route::get('/jobs/create', function () {
-	return view('create-job');
+	return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function (int $id) {
 
 	$job = Job::find($id);
 
-	return view('job', ['job' => $job]);
+	return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
