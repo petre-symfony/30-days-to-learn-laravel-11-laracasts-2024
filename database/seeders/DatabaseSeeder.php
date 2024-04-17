07a847808cd15543e57c00e1185e1c4da2a3591a
petrero <petrero@gmail.com>
2024-04-17 15:15:27 +0300

    15.2 Okay, so maybe when you seed your database, you want a single user, and then we want some jobs. So I can say job factory, let's do 200. And create. Alright, so let's try this.  Now keep in mind, though, if I run  php artisan db:seed  , it's going to fail. So can you can you guess why? Actually, before I show you, can you guess why did that fail? Alright, well, it failed because of a unique constraint. Of course, a user's email address has to be unique. The problem is we're trying to add a new john doe with an email of tested example, but that already exists. So this is something you should be aware of. If you're going to run that db:seed commands, you either need to clear out all of the previous records, or combine it with migrate fresh, something to be aware of.  php artisan migrate:fresh --seed. Okay, and now we avoid that problem. We have one user and sorry, job listings, we have 200 random jobs, it works.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -19,5 +20,7 @@ class DatabaseSeeder extends Seeder {
 			'last_name' => 'Doe',
 			'email' => 'test@example.com',
 		]);
+
+		Job::factory(200)->create();
 	}
 }
