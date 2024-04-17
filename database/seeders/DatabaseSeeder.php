2271125575b4502c31e7a67f3d27b90e920c1601
petrero <petrero@gmail.com>
2024-04-17 15:08:10 +0300

    15.1 Understanding Database Seeders  Alright, let's talk about database seeding today. So imagine that you need to refresh your database. You can run php   artisan migrate:fresh.  Alright, so that drops all of your tables and builds them up from scratch. But if I switch back to the browser, now, of course, we don't see any records at all. So think about it, what would we have to do at this point? Well, we'd have to boot up php artisan tinker. And then once again, I could say App\Models\Job::factory(100)->create(), we would manually have to run a command to build up this table.  And then I'd have to do it for all of the various tables for my application. It's not ideal. Ideally, I'd run php artisan migrate:fresh, and then see the database populate all of those tables, however I require. Alright, well, as it turns out, we can do this through database seeders. So check this out. Within my editor, let's open up the database and a new folder seeders. And we have a single file to get us started database seeder. And notice they give us some examples. Do you want to create 10 dummy user records or a single user record? Alright, very cool. How do we execute this? How do we trigger this?  Well, from the terminal, you can run   php artisan db:seed.  And remember, if you ever forget, you can just run artisan and then do a command F and just look for the command. And then you can find it. Ah, this is the one we want seed the database with records. Okay, so let's run it.   php artisan db:seed.  Oh, it fails. What did I do wrong?   Table users has no column named name.  It's the exact same problem we found ourselves in a couple episodes ago. Yes, once again, we changed it to first name, and I'm just going to do john. And then last name will be doe. Alright, try it one more time. Sorry about that.  There we go. So we seeded the database. If I now have a look within the users table, sure enough, we have john doe. Great. It works. So now think about this.  If I were to run   php artisan migrate:fresh  , well, we've learned that drops all of the tables and builds them up from scratch. But then, as a result, we lose all of our seeded data. But check this out. If I run it again, but I add the help command before it,   php artisan help migrate:fresh  ,this will show me all of the various options that we can pass. And one of them is seed indicates if the seed task should be rerun. So this effectively means refresh the database and then run php artisan db seed. But we can do that as part of a single command.   php artisan migrate:fresh --seed.  So it drops the tables, it runs the migrations, and then it seeds the database all in one go, come back, refresh, and it works. Very cool.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void {
+		// User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+		User::factory()->create([
+			'first_name' => 'Joe',
+			'last_name' => 'Doe',
+			'email' => 'test@example.com',
+		]);
+	}
 }
