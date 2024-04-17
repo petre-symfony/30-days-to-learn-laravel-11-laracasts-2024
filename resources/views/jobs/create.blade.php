69e1a7f5577e8610a71a170c3d1a182f087090c5
petrero <petrero@gmail.com>
2024-04-17 15:59:54 +0300

    16.4 Okay, so now let's go into our create view, and we'll update the heading to create job, and then we'll say to do. Alright, let's have a look in the browser. Alright, so here is the index view to display all jobs. Here's the show view to show a single job. And then we have a new one, create, will display a form to create a new job.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,8 +1,7 @@
 <x-layout>
-	<x-slot:heading>Job</x-slot:heading>
+	<x-slot:heading>Create Job</x-slot:heading>
 
-	<h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
 	<p>
-		This job pays {{ $job['salary'] }} per year
+		TODO
 	</p>
 </x-layout>
