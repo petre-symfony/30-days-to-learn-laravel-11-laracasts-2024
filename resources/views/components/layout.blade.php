dad8beca10def448bd354a53d3762be73ac15f18
petrero <petrero@gmail.com>
2024-04-19 14:14:51 +0300

    17.2 Okay, so if this does represent our default button styling, I'll show you how to quickly extract that component, create a new file in the components directory. I'm going to call it something simple, button, or link, whatever you want. I'll call it button. Alright, switch back, and I'm going to copy the whole thing, move it in.  Alright, next, of course, I don't want to hard code the href, that'll be passed in. But I do want a default set of CSS classes. So here's what I'll do. I'm going to copy those classes, and then say attributes, merge, and set the initial classes to that long string. Okay, and then of course, we're not going to hard code create job, this will be our slot.  And that should do it. Alright, so let's come back to our layout. And now this can be swapped out with x button component, the href will be jobs slash create, create job. Yeah, it should work. So come back, refresh, and click on it, and we're all set to go.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -98,16 +98,7 @@
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
 
-        <a
-            href="/jobs/create"
-            class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500
-              bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10
-              focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100
-              active:text-gray-500 transition ease-in-out duration-150 dark:bg-gray-800
-              dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800
-            "
-        >Create Job
-        </a>
+        <x-button href="jobs/create">Create Job</x-button>
       </div>
     </header>
     <main>
