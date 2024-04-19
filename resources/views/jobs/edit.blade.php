53c18a3f6b65c12dc768a433f0682acbe57e41bb
petrero <petrero@gmail.com>
2024-04-19 16:01:50 +0300

    18.2 And if we have a look here, we'll change this to edit job. And then I'll expect to have a job object. So I will say edit job and then the title of the job. And then let's get rid of this section right here.  And yeah, we'll have a look in the browser. So if I go back to my show view, I can update the href to jobs slash the ID of the job slash edit. But actually before I do that, notice how we're now accessing our attributes in two different ways.  So here I'm accessing them as properties, but up here I'm accessing them as array keys. And that's because this example was created before we switched over to Eloquent. Now Eloquent doesn't care how you access those attributes. You can use them as array keys or properties, it doesn't matter. You'll get the exact same thing. But of course, you want to be consistent. So with that in mind, I'm going to swap these over. This is what you will typically see in the wild.  All right, so let's come back to the browser, give it a refresh. And now if I click edit job, we're brought to a new page to edit the secretary position.

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -1,16 +1,10 @@
 <x-layout>
-  <x-slot:heading>Create Job</x-slot:heading>
+  <x-slot:heading>Edit Job: {{ $job->title }}</x-slot:heading>
 
   <form method="POST" action="/jobs">
     @csrf
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">
-          Create a New Job
-        </h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">
-          We just need a handful of details from you
-        </p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
