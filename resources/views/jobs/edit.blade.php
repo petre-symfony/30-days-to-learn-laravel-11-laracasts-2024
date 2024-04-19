1c76028f05a6f5675988521558c938166b3b3e48
petrero <petrero@gmail.com>
2024-04-19 17:32:38 +0300

    18.12 Okay, so now the only remaining step is to add a button to delete the job. So maybe we'll handle that within this edit form.  What I'm imagining is right down here, a little red link or button that says delete. All right, let's do that now. Let's go back to edit.played, and let's see, let's scroll down to this section.  So this div corresponds to what you see here. So it sounds like I will need two different sections, one for the left side and one for the right side. So what I will have to do, let's clean this up, is once again set flex and item center and gap x6 on this wrapper. And often when I'm using Flexbox, I'm not sure what the actual rules are, but I have found that you can sometimes end up with weird button issues if they are direct children of a flex container.  So I almost always wrap those within divs. Like I said, I'm not sure what the real rule is there, but it's just something I've encountered more than once. So yeah, I still see that there, and that looks good.  Next I will have a button for delete. Come back and refresh. But now it's all the way over there, and that's because on the main container we have justify end. I'm going to change that to justify between. Come back, refresh, and now it's over here. Cool.  So now let's style it. Let's make it red, small, and bold. Yep. But now notice if I line these up, the formatting isn't quite right, or the alignment isn't quite right. So on this one as well, I will set flex and items center, because maybe in the future we will have more than one link there. All right. That looks good. And yeah, if we use a quick ruler here, it lines up.Okay.

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -4,7 +4,7 @@
   <form method="POST" action="/jobs/{{ $job->id }}">
     @csrf
     @method('PATCH')
-    
+
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
 
@@ -66,16 +66,23 @@
 
     </div>
 
-    <div class="mt-6 flex items-center justify-end gap-x-6">
-      <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-      <button type="submit"
-        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm
-          hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
-          focus-visible:outline-offset-2 focus-visible:outline-indigo-600
-        "
-      >
-        Update
-      </button>
+    <div class="mt-6 flex items-center justify-between gap-x-6">
+      <div class="flex items-center">
+        <button class="text-red-500 text-sm font-bold">Delete</button>
+      </div>
+      <div class="flex items-center gap-x-6">
+        <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+        <div>
+          <button type="submit"
+            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm
+              hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+              focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+            "
+          >
+            Update
+          </button>
+        </div>
+      </div>
     </div>
   </form>
 
