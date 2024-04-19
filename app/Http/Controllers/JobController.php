32c1ffdc30e9069096f1ef1c1cfc4375c4787591
petrero <petrero@gmail.com>
2024-04-19 21:30:09 +0300

    19.5 So now think about it. Let's uncomment this out. All I have to do is take the contents of this closure and move it over into this new home, and then I can delete this entirely.  Okay, next, I, of course, will import the job class. All right, so now, yeah, we've done our first successful migration from a route closure to a dedicated controller action, and that's the term we would use to describe each one of these. They are actions on the controller. Come back, refresh, and it works. So cool.  Okay, now I will go down the list and update all of these, and I'll do it a little bit quicker. So for the create action, I can cut and paste that in. For the show action, once again, cut, paste it in, but what about this section here? Can I just move that over? And the answer is, yep, you got it. All right, get rid of that. All right, next, store action. I will cut all of that, paste it in. What else? We have edit, add our type, and then update. Yeah, a little bit tedious, but it's just for demonstration. All right, job, job, and then finally, we have destroy. All right, very cool.  So our controller is looking good.  I will close that out, and then I just want to update all of these other routes. So I can say we have job, controller, class. This time we're calling the create action. Okay, and what I'll do here is select all of that so I can save myself some time. All right, paste it in. This one is show. This one is store. This one is edit. And this one is update. And finally, we have destroy.  Yeah, a little bit cleaner, don't you think? Definitely easier to reason about and scroll. Okay, so now I can get rid of all of these comments. That's redundant. And this is what we get. Now it's crystal clear that these are the routes related to our jobs. And also, by the way, I can get rid of this job import.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,34 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class JobController extends Controller {
 	public function index() {
-		dd('hello');
+		$jobs = Job::with('employer')->latest()->simplePaginate(3);
+
+		return view('jobs.index', [
+			'jobs' => $jobs
+		]);
 	}
 
 	public function create() {
-		
+		return view('jobs.create');
 	}
 
-	public function show() {
-		
+	public function show(Job $job) {
+		return view('jobs.show', ['job' => $job]);
 	}
 
 	public function store() {
-		
+		request()->validate([
+			'title' => ['required', 'min:3'],
+			'salary' => ['required']
+		]);
+
+		Job::create([
+			'title' => request('title'),
+			'salary' => request('salary'),
+			'employer_id' => 1
+		]);
+
+		return redirect('/jobs');
 	}
 
-	public function edit() {
-		
+	public function edit(Job $job) {
+		return view('jobs.edit', ['job' => $job]);
 	}
 
-	public function update() {
-		
+	public function update(Job $job) {
+		//authorize (on hold...)
+		request()->validate([
+			'title' => ['required', 'min:3'],
+			'salary' => ['required']
+		]);
+
+		$job->update([
+			'title' => request('title'),
+			'salary' => request('salary')
+		]);
+
+		return redirect('/jobs/' . $job->id);
 	}
 
-	public function destroy() {
-		
+	public function destroy(Job $job) {
+		//authorize(on hold...)
+		$job->delete();
+
+		return redirect('/jobs');
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,71 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', [JobController::class, 'index']);
-// index
-// Route::get('/jobs', function () {
-// 	$jobs = Job::with('employer')->latest()->simplePaginate(3);
-// 	return view('jobs.index', [
-// 		'jobs' => $jobs
-// 	]);
-// });
-
-//form
-Route::get('/jobs/create', function () {
-	return view('jobs.create');
-});
-
+Route::get('/jobs/create', [JobController::class, 'create']);
 Route::get('/posts/{post:slug}', function () {});
-
-//show
-Route::get('/jobs/{job}', function (Job $job) {
-	return view('jobs.show', ['job' => $job]);
-});
-
-//store
-Route::post('/jobs', function (){
-	request()->validate([
-		'title' => ['required', 'min:3'],
-		'salary' => ['required']
-	]);
-
-	Job::create([
-		'title' => request('title'),
-		'salary' => request('salary'),
-		'employer_id' => 1
-	]);
-
-	return redirect('/jobs');
-});
-
-//sdit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-
-	return view('jobs.edit', ['job' => $job]);
-});
-
-//update
-Route::patch('/jobs/{job}', function (Job $job) {
-	//authorize (on hold...)
-	request()->validate([
-		'title' => ['required', 'min:3'],
-		'salary' => ['required']
-	]);
-
-	$job->update([
-		'title' => request('title'),
-		'salary' => request('salary')
-	]);
-
-	return redirect('/jobs/' . $job->id);
-});
-
-//destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-	//authorize(on hold...)
-	$job->delete();
-
-	return redirect('/jobs');
-});
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/contact', function () {
 	return view('contact');
