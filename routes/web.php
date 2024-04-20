f1a251e48b779d2b090b256d6f61ab7ce0daa579
petrero <petrero@gmail.com>
2024-04-20 21:17:24 +0300

    19.9 But now that brings us to our next technique, which is route resource. So yeah, multiple times in this video, I have used that term resource to represent this collection here of actions.  So as it turns out, Laravel has its own dedicated route resource method. I'll show you how to use it. The first argument will be the resource name. This will also be the URI, in this case, jobs. The next argument is the controller that is responsible for it, job controller.  Okay, so check this out. If I comment out this line here and I rerun route list, notice I get the exact same thing because that's what route resource does. It registers all of the routes for a typical restful or resourceful controller. And notice the convention that we're following here.  If I didn't have any of these here, and I just had an empty controller, if I run route list, I still see that it's going to assume and default to these action names. And yeah, on that note, this is specifically why I've tried to be crystal clear about these action names over the last few episodes.  And it's also why we added a little comment above each route declaration two episodes ago. But yeah, if you can't remember them, you got to do it. Just practice. Close your eyes and say them out loud. Index, show, create, store, edit, update, destroy, index, show. Just do it over and over until it rolls off your tongue.

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,16 @@ Route::view('/','home');
 
 Route::get('/posts/{post:slug}', function () {});
 
-Route::controller(JobController::class)->group(function (){
-	Route::get('/jobs', 'index');
-	Route::get('/jobs/create', 'create');
-	Route::get('/jobs/{job}', 'show');
-	Route::post('/jobs', 'store');
-	Route::get('/jobs/{job}/edit', 'edit');
-	Route::patch('/jobs/{job}', 'update');
-	Route::delete('/jobs/{job}', 'destroy');
-});
+//Route::controller(JobController::class)->group(function (){
+//	Route::get('/jobs', 'index');
+//	Route::get('/jobs/create', 'create');
+//	Route::get('/jobs/{job}', 'show');
+//	Route::post('/jobs', 'store');
+//	Route::get('/jobs/{job}/edit', 'edit');
+//	Route::patch('/jobs/{job}', 'update');
+//	Route::delete('/jobs/{job}', 'destroy');
+//});
+
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
