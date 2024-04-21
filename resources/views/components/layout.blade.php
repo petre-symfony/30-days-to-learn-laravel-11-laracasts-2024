3702e6c4b52c2725a1968510996be5137305358e
petrero <petrero@gmail.com>
2024-04-21 17:41:25 +0300

    21.11 Last thing I want to do here is if I click on my nav link component, we can pass through an active status. So yeah, just like we did with home and jobs, we should provide some additional styling if you are on that page. So if I come back for login, I can say, make this one active if the request is login.  And then I'll do the same thing for register, like so. Now if I switch back and refresh, yeah, we just have a little extra feedback, which can be helpful. Okay.  I think we're making pretty good progress here.  All right. I get it. I know what you're thinking. I can read your mind. This was definitely, absolutely, certainly one of the more boring episodes in the course.  But folks, what do you want me to do? It's form code. It has to be written. My hands are tied here. We had to do it. And we did do it. So good job. We made it through this lesson.  And now in the next episode, we can focus on what I consider to be some more fun logic. So stay tuned for that. You're halfway through. Let's keep going.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,8 +28,8 @@
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
               @guest
-                <x-nav-link href="/login">Log In</x-nav-link>
-                <x-nav-link href="/register">Register</x-nav-link>
+                <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+                <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
               @endguest
             </div>
           </div>
