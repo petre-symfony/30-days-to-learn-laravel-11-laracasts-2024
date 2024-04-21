aa6b6e198effb68427f8c961382e472502ac66d4
petrero <petrero@gmail.com>
2024-04-21 20:43:05 +0300

    22.10 Okay, so yeah, why don't we just take care of this right now because after that, we can start working on the login functionality. So if we can log in a user by saying login user, then we can log out a user by saying log out. And in this case, I don't have to provide a user because it assumes the currently authenticated user.  All right, so let's import that. And then finally, after we log out a user, let's redirect them to the homepage. That sounds good to me.  All right, so let's give it a shot. Come back, refresh. I'm on the jobs page. I click log out. I'm redirected to the homepage. I'm no longer authenticated. So now I once again see the login and register links. Cool.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller {
 	public function create() {
@@ -14,6 +15,8 @@ class SessionController extends Controller {
 	}
 
 	public function destroy() {
-		dd('log the user out');
+		Auth::logout();
+
+		return redirect('/');
 	}
 }
