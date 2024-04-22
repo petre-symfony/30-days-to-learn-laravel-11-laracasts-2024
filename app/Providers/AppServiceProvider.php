9a0158abf177e5332b535176e92af01b8a154356
petrero <petrero@gmail.com>
2024-04-22 11:37:39 +0300

    23.14 In plenty of situations, a single method will do just fine. So if I select all of these, I could say, well, the only one I have at the moment is edit. Can you edit a job? This accepts the user and the job, just like before. So notice, this is almost like a version of a gate. It's sort of the same thing, just a different interface for it. So with that in mind, if I open up app service provider, let's take this logic here. I will comment this out. And if I switch back, I will paste it in here. Now, once again, just like your gate definition, this is going to return a Boolean. So if you want, you can add a return type to it. Alright, so check this out. Now that we've migrated from a simple gate closure to a dedicated policy, now we can update our code. Let's go to the routes file. Back to our edit route. Yeah, at this point, we no longer have a gate or a policy called edit job. Ours is called edit. So notice we reference the method name on the policy, and then we feed it the corresponding model. So now Laravel is smart enough to know there is a link between your job model and your job policy. So now we're just saying, alright, find the policy for a job, and then run the edit authorization on that policy. And this will work just like it did before. Reload the page. If I try to hit that edit endpoint, of course, I get unauthorized. Very cool. Now further, keep in mind, if I go back to the job that we did create this one, I don't see that button. And that's because again, we have to update our view. Let's go to jobs slash show. And yet once again, we no longer have a gate with this name, it's simply called edit. And I think that reads just a little bit better. So now, back to the browser, give it a refresh. And it works just like it did before. All right, and that's a wrap. So yeah, you see what I mean. There's so much flexibility in Laravel when it comes to authorization that I don't know, sometimes it can lead to decision paralysis. Do I handle authorization in my routes file or in my controller? Do I use the gate facade? Or do I create some policies? And as always, the answer is, it just sort of depends on what you want to do. Here's my recommendation. If you're building something relatively simple, the gate facade is fine. Open your app service provider, define a few gates, and you're good to go. For anything a little bit larger, I would 100% recommend that you stick with policies. And by the way, that's what I do for Laracast. Okay, that's it. I will see you tomorrow. Please be excited.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,5 @@ class AppServiceProvider extends ServiceProvider {
 
 		//Paginator::defaultView();
 		//Paginator::useBootstrapFive();
-
-		Gate::define('edit-job', function (User $user, Job $job) {
-			return $job->employer->user->is($user);
-		});
 	}
 }
