b9c291e3c0a0cc72bc238bc6364516e6be8a5019
petrero <petrero@gmail.com>
2024-04-22 10:29:25 +0300

    23.5 All right, that brings us to step three. Think about it right now, this gate definition will only be created if you're on the edit page. So if I switch back to the browser, this is the show view for a job. And yet at this point, that edit job gate does not exist because it hasn't been loaded yet for the request. All right, so that doesn't make any sense. Let's do this. Let's take our gate definition and move it to a place where it's available for every single request. A good place for that is your app service provider. And I will paste it here. Cool. So now I have a place to define simple bits of authorization that can be referenced anywhere in my application. Now one quick note before we move on, this is important to remember, the user object here that Laravel passes into your closure will always reference the currently authenticated user. But what about situations where you're a guest, you're not signed in? Well, in those cases, the gate will always fail, you will never be authorized, it won't even bother hitting this logic, it just immediately returns false. Now in some situations, you may not want that. You can either fix it by setting user to null, or you can make the type optional. In either of these cases, it will allow you to trigger this logic even if you're not signed in. Okay, in our case, we want a signed in user. So check this out. If I come back to job controller, this logic now that confirms you're signed in is irrelevant. I can remove it entirely. Very cool.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -22,5 +25,9 @@ class AppServiceProvider extends ServiceProvider {
 
 		//Paginator::defaultView();
 		//Paginator::useBootstrapFive();
+
+		Gate::define('edit-job', function (User $user, Job $job) {
+			return $job->employer->user->is($user);
+		});
 	}
 }
