6a8ad2c2d1d35540eec91ba6bf7934d2e7954def
petrero <petrero@gmail.com>
2024-04-22 10:48:17 +0300

    23.9 So we can fix this by returning to our routes file and giving this particular route a name. And now that'll do the trick. If I come back and refresh, it works. So just a little aside, if you want to learn more about that research Laravel named routes (laravel.com/docs/master/routing#named-routes).

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::resource('jobs', JobController::class)->middleware('auth');
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
