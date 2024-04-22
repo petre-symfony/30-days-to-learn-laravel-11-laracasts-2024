64cdf9517cd7cf52bb9592ba291bb108c5bdb590
petrero <petrero@gmail.com>
2024-04-22 10:23:13 +0300

    23.4 Alright, that brings us to step two, gates. So think about it. At the moment, we are still displaying this edit job button, even though you're not authorized to see it. And that doesn't make any sense. The only problem is, well, the logic that determines whether or not you're authorized is stuck inside of this controller action. And that's not very helpful. Okay, so let's incrementally fix this. I'm going to use a gate facade that Laravel ships with out of the box. Illuminate, support, facades, gate. Now think of a gate like a real life gate. It's a barrier. And it conditionally allows entry if you meet certain criteria, right? So if you are authorized, that gate will open. But if you are unauthorized, the gate is locked. So let's define a gate. And we can give this any name we want. How about edit job. And we'll pass a function here that will accept the currently signed in user as well as the job that we are authorizing. Okay, so now within here, we should return a Boolean that indicates whether or not this user is authorized to edit that job. All right, well, we could take all of this here and move it in. But like I said, it doesn't need to abort, it just needs to return a Boolean. So I can shorten this and say, well, return if the job employer user is the currently signed in user. And of course, I can replace that with user. Cool. So does that make sense? We are taking that logic and extracting it into its own gates. The gate is called edit job. And the logic for whether you can edit a job is well look at the user responsible for the job and see if that user is the same as the user who is currently signed in. Okay, so now I can reference that logic by saying gate, authorize, we provide the name, edit job, and then we pass in the job that we are referring to. All right, let's give it a shot. Back to the browser, edit job, and we get a 403. All right, so this is important to understand the authorized method will run the logic associated with the name that you reference. And if it fails or returns false, Laravel automatically aborts with a 403. Now there might be situations where you don't want it to do that. In these cases, you can manually run a conditional, you could run   Gate:allows or Gate:denies.  So for example, if you have a situation where you don't want to abort, but instead do something else, log something redirect somewhere, you could say   if (Gate:denies('edit-job')), then I will personally handle that response. But yeah, otherwise, if you're fine with Laravel doing it, then you run the gate authorize method.

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller {
 	public function index() {
@@ -39,13 +41,15 @@ class JobController extends Controller {
 	}
 
 	public function edit(Job $job) {
+		Gate::define('edit-job', function (User $user, Job $job) {
+			return $job->employer->user->is($user);
+		});
+
 		if(Auth::guest()){
 			return redirect('/login');
 		}
 
-		if ($job->employer->user->isNot(Auth::user())) {
-			abort(403);
-		}
+		Gate::authorize('edit-job', $job);
 
 		return view('jobs.edit', ['job' => $job]);
 	}
