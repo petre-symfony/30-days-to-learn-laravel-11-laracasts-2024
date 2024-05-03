db8b9d5b470a803be0423e1ed0971c0837aac43b
petrero <petrero@gmail.com>
2024-05-03 17:40:19 +0300

    24.11 So you need to provide the full URL, we actually want example dot test slash jobs slash job ID.  So in these situations, you can update it to URL to jobs slash job ID. Yeah, if we command click here, that will generate a URL for the application. And you'll see what shape that takes shortly.  Finally, maybe at the top, it's not gonna look super pretty, but maybe within an h2, we will have the title of your job. Alright, and I think that's enough for an example.  Let's send the email again. All right, back to our project, I will need to log in.  All right, john will create a new job, Lara Cass video producer pays 90,000 per year, save it, that will persist it in the database. Notice it took a second or so, but I can see it here. And now if we switch back to MailTrap, sure enough, we have a confirmation email.  But yeah, sure enough, we have the title of the job. Congrats, your job is now live, view it. And if I click on it, notice it takes me to that full URL to our project. And by the way, what's useful about that is URL will work for your local environment as well as your production environment. Alright, so that's a wrap for our mail lesson, at least mostly. Now you'll remember, I kept bringing up the fact that it does take a bit of time to deliver an email. And in a production environment that's even longer. So yeah, if you think about it, it's just not practical to make the user wait two, three, maybe four seconds for an email to send. I'm waiting. When on top of that, they may not even know you're delivering them an email, they're just publishing a job. They don't know that as part of that request, you're also preparing and firing off an email. So yeah, we don't want to do this. So we can improve performance by instead, throwing the email onto what's known as a queue. And if you don't know what a queue is, then of course, you're in luck, because that is the topic of tomorrow's lesson. So I'll see you then.

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,7 +1,9 @@
+<h2>{{ $job->title }}</h2>
+
 <p>
   Congrats! Your job is now live on our website.
 </p>
 
 <p>
-  <a href="/jobs/{{ $job->id }}">View Your Job Listing</a>
+  <a href="{{ url('/jobs/' . $job->id) }}">View Your Job Listing</a>
 </p>
