2e6a6949b52c3117e4aae51b8cc575c8a970049a
petrero <petrero@gmail.com>
2024-05-04 19:17:02 +0300

    26.3 So now if I switch over to the browser, yeah, I don't see any red here. Do I? Well, of course not. We defined it in that style sheet, but we haven't yet imported it within our blade files.  So that's the next step. So back in our editor, let's go up into our views directory, and of course, we could place this within our layout component. I'll do it right here at the top. Now Laravel has a vite directive that makes this really easy.  All I have to do is reference that root app.css file, and it'll take care of the rest. All right, and now we get red. Perfect.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>{{ $heading }}</title>
+  @vite(['resources/css/app.css'])
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
