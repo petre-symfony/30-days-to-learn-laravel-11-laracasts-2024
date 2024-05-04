807d8542c18da44f2e7a842e0c28b764d73361d8
petrero <petrero@gmail.com>
2024-05-04 19:35:31 +0300

    26.5 Back to vite.config, we specified an entry point as resources.js.app.js, so you will find that here. All right, so this is what Laravel suggests out of the box.  Maybe import a bootstrap file that does anything you need to bootstrap your front-end dependencies. In this case, because making AJAX requests is so common, Laravel will assume, eh, maybe you probably want to pull in Axios and provide some initial configuration.  All right, so let's do, you know, some Hello World stuff here. Let's alert hello from the JS. Save the file. All right, and now, of course, we need to reference it, right?  So we can do that once again from our layout file, and we can reuse the same vite directive, resources.js.app.js. All right, so immediately when I save this, the browser should update.  So if I switch back, all right, and there we go. So now we are bundling up both our CSS and our JavaScript files.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>{{ $heading }}</title>
-  @vite(['resources/css/app.css'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
